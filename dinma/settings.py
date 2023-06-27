d12602347e81ecfc74234ddd51320a7c1821ddad
AMOO <amoosamson@gmail.com>
2023-06-27 21:57:20 +0100

    restfull api

--- dinma/settings.py
+++ dinma/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-u4cg94=@l-6ouze+n55r#&9xkqpb&cbu@6^)ddq&msoli_^f+s'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))  
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
